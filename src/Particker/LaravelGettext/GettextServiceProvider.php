a0859087ec9eafbef1253f637dd7a3bc47c4cd31
Particker <wangshanliang13145@163.com>
2017-08-22 11:49:41 +0800

    去除gettext扩展,利用本地PHP扩展完成多语言支持

--- src/Particker/LaravelGettext/GettextServiceProvider.php
+++ src/Particker/LaravelGettext/GettextServiceProvider.php
@@ -2,7 +2,7 @@
 namespace Particker\LaravelGettext;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Input;
+
 class GettextServiceProvider extends ServiceProvider
 {
     /**
@@ -29,36 +29,14 @@ class GettextServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        return $this->load(config('gettext'));
-    }
-    public function load(array $config)
-    {
-        if (empty($config)) {
-            return;
-        }
-        $config['storage'] = base_path($config['storage']);
-        foreach ($config['directories'] as $key => $directory) {
-            $config['directories'][$key] = base_path($directory);
-        }
-        $cookie = $config['cookie'];
-        $path = parse_url(url('/'), PHP_URL_PATH) ?: '/';
-        $_COOKIE[$cookie] = isset($_COOKIE[$cookie]) ? $_COOKIE[$cookie] : null;
-        $gettext = new Gettext($config);
-        $gettext->setLocale($_COOKIE[$cookie], Input::get($cookie));
-        $gettext->load();
-        setcookie($cookie, $_COOKIE[$cookie] = $gettext->getLocale(), (time() + 3600 * 24 * 30 * 12), $path);
-        $this->app->singleton('gettext', function() use ($gettext) {
+        $dir = config('gettext.directories');
+        $domain = config('gettext.domain');
+        $lang = config('gettext.locales');
+        $charset = config('gettext.charset');
+        $gettext =  new Gettext($dir, $domain,$lang,$charset);
+        
+        $this->app->singleton('gettext', function($app) use ($gettext) {
             return $gettext;
         });
-            return $gettext;
-    }
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['gettext'];
     }
 }
